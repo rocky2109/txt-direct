5ae8fbc17c55495b265eae4d03b382dcc3cf8b31
Abhi Dev <168731980+abhishek84556@users.noreply.github.com>
2025-05-30 08:59:10 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,10 +3,10 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "28094744"))
-API_HASH = environ.get("API_HASH", "a75af4285edc7747c57bb19147ca0b9b")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "5680454765"))
+API_ID = int(environ.get("API_ID", "29028535"))
+API_HASH = environ.get("API_HASH", "9e501cebfbd5da42d6b74d48a1bf2536")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7692540490:AAGOnamVpEAT8QT7dA0SjF4Y-nsxmXgwImU")
+OWNER = int(environ.get("OWNER", "5913865424"))
 CREDIT = "𝄟⃝‌🐬🇳‌ɪᴋʜɪʟ𝄟⃝🐬"
 AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
